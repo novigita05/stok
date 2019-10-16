915375dbd5ae0681d3a62767dad6d08b40a9959f
novigita05 <48269480+novigita05@users.noreply.github.com>
2019-10-16 09:26:04 +0700

    Add files via upload

--- app/Controllers/Kategori.php
+++ app/Controllers/Kategori.php
@@ -19,6 +19,22 @@ class Kategori extends BaseController {
         );
         view_page($data);
     }
+    
+    public function edit($id)
+    {
+        $text1 = $this->request->getPost('text1');
+        $text2 = $this->request->getPost('text2');
+        $arr = array(
+            'kategori' => $text1
+        );
+        // echo "$text1 - $text2";
+        $this->db->table('kategori')->update($arr,'id_kategori = '.$id);
+        if ($this->db->affectedRows() > 0) {
+            echo "sukses";
+        }else{
+            echo "gagal";
+        }
+    }
     public function get_json()
     {
         header('Content-Type:application/json');
@@ -33,15 +49,29 @@ class Kategori extends BaseController {
             $hapus = '<button type="button" class="btn btn-icon btn-danger mr-1" onclick="load_modal(`hapus`, `kategori`, `'.$value->id_kategori.'`, `'.$value->kategori.'`)">delete</button>';
         
             $data['aksi'] = $edit.$hapus;
+            
             array_push($content, $data);
         }
         echo json_encode($content);
     }
+
+    public function hapus($id){
+        $data=array(
+            'kategori' => $kategori
+        );
+
+        $this->db->delete('kategori', $data);
+        if($this->db->affectedRows() > 0){
+            echo "sukses";
+        }else{
+            echo "gagal";
+        }
+    }
+
     public function cu_kategori()
     {
         $id = $this->request->getPost('id');
         $data = $this->request->getPost();
-        $arr = array();
         foreach ($data as $key => $value) {
             if ($value!=="") {
                 $arr[$key] = $value;
@@ -57,37 +87,5 @@ class Kategori extends BaseController {
         }else{
             echo "gagal";
         }
-    }
-    public function create() {
-        helper('kategori/form');
-
-        echo view('kategori/form');
-    }
-
-    public function store() {
-        helper('form');
-
-        $model = new Kategori_model();
-
-        DB::table('kategori')->insert([
-            'kategori' => $request->kategori
-        ]);
-
-        return redirect('kategori/kategori');
-    }
-
-    public function edit($id = null) {
-        $model = new Kategori_model();
-
-        $data['kategori'] = $model->getKategori($id);
-
-        echo view('kategori/edit',$data);
-    }
-
-    public function delete($id = null) {
-        $model = new Kategori_model();
-        $model->delete($id);
-
-        return redirect()->to(base_url('kategori'));
     }
 }
